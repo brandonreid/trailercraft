c4d3056f7e84ab14d08eb59f0dab191838ad4995
Brandon Reid <breid@criticalmix.com>
2018-09-27 08:22:42 -0700

    Inerum Commit

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,18 @@ import Navbar from '../components/navbar/Navbar';
 import Cosmic from 'cosmicjs';
 import css from './index.scss';
 
-const Index = ({data, heroWidth, advantageImgWidth}) => (
+const Index = ({data, imgWidths}) => (
   <div>
     <Navbar />
     <section
       className={`${css.homeHero} hero red-gradient`}
       style={{
-        backgroundImage: `url(${data.home_hero_image.imgix_url}?w=${heroWidth})`
+        backgroundImage: `url(${data.home_hero.home_hero_image.imgix_url}?w=${imgWidths.heroWidth})`
       }}>
       <div className={css.homeHeroText}>
-        <h2>{ data.home_hero_title }</h2>
-        <p>{ data.home_hero_text }</p>
+        <h2>{ data.home_hero.home_hero_title }</h2>
+        {data.home_hero.home_hero_text
+          && <p>{ data.home_hero.home_hero_text }</p>}
       </div>
       <svg><use xlinkHref="#chevron" /></svg>
     </section>
@@ -26,35 +27,35 @@ const Index = ({data, heroWidth, advantageImgWidth}) => (
     <section
       className={`${css.coreValue} hero top-left-pin`}
       style={{
-        backgroundImage: `url(${data.core_value_background_image.imgix_url}?w=${heroWidth})`
+        backgroundImage: `url(${data.core_value.core_value_background_image.imgix_url}?w=${imgWidths.heroWidth})`
       }}>
       <div className={css.valueText}>
         <svg><use xlinkHref="#chevron" /></svg>
-        <h2>{ data.core_value_title }</h2>
-        <p>{ data.core_value_text }</p>
+        <h2>{ data.core_value.core_value_title }</h2>
+        <p>{ data.core_value.core_value_text }</p>
       </div>
     </section>
     <section
       className={`${css.advantagesIntro} hero red-gradient`}
       style={{
-        backgroundImage: `url(${data.advantages_intro_background_image.imgix_url}?w=${heroWidth})`
+        backgroundImage: `url(${data.advantages_intro.advantages_intro_background_image.imgix_url}?w=${imgWidths.heroWidth})`
       }}>
       <svg><use xlinkHref="#chevron" /></svg>
       <div className={css.advantageText}>
-        <h2>{ data.advantages_intro_header }</h2>
-        <p>{ data.advantages_intro }</p>
+        <h2>{ data.advantages_intro.advantages_intro_header }</h2>
+        <p>{ data.advantages_intro.advantages_intro }</p>
       </div>
     </section>
     <section className={css.advantages}>
       <div className={css.advantage}>
-        <img src={ `${data.advantages.advantage_1_image.imgix_url}?w=${advantageImgWidth}` } alt="" />
+        <img src={ `${data.advantages.advantage_1_image.imgix_url}?w=${imgWidths.advantageImgWidth}` } alt="" />
         <div className={css.advantageText}>
           <h3>{ data.advantages.advantage_1_header }</h3>
           <p>{ data.advantages.advantage_1_text }</p>
         </div>
       </div>
       <div className={css.advantage}>
-        <img src={ `${data.advantages.advantage_2_image.imgix_url}?w=${advantageImgWidth}` } alt="" />
+        <img src={ `${data.advantages.advantage_2_image.imgix_url}?w=${imgWidths.advantageImgWidth}` } alt="" />
         <div className={css.advantageText}>
           <h3>{ data.advantages.advantage_2_header }</h3>
           <p>{ data.advantages.advantage_2_text }</p>
@@ -65,14 +66,60 @@ const Index = ({data, heroWidth, advantageImgWidth}) => (
           <h3>{ data.advantages.advantage_3_header }</h3>
           <p>{ data.advantages.advantage_3_text }</p>
         </div>
-        <img src={ `${data.advantages.advantage_3_image.imgix_url}?w=${advantageImgWidth}` } alt="" />
+        <img src={ `${data.advantages.advantage_3_image.imgix_url}?w=${imgWidths.advantageImgWidth}` } alt="" />
       </div>
       <div className={css.advantage}>
         <div className={css.advantageText}>
           <h3>{ data.advantages.advantage_4_header }</h3>
           <p>{ data.advantages.advantage_4_text }</p>
         </div>
-        <img src={ `${data.advantages.advantage_4_image.imgix_url}?w=${advantageImgWidth}` } alt="" />
+        <img src={ `${data.advantages.advantage_4_image.imgix_url}?w=${imgWidths.advantageImgWidth}` } alt="" />
+      </div>
+    </section>
+    <section className={css.callToAction}>
+      <h2>{ data.call_to_action.call_to_action_header }</h2>
+      <p>{ data.call_to_action.call_to_action_subtext }</p>
+      <div className={css.ctaLocations}>
+        <div className={css.ctaLocationContainer}>
+          <p>
+            <strong>Anchorage</strong>
+            <a className={css.locationPhone}
+               href={`tel: ${data.call_to_action.anchorage_phone_number}`}
+               title="Click to call Anchorage location.">
+              { data.call_to_action.anchorage_phone_number }
+            </a>
+            <a className={css.locationMap}
+               href={data.call_to_action.anchorage_google_maps_link}>
+              <img
+                src={`${data.call_to_action.anchorage_map_image.imgix_url}?w=${imgWidths.mapImgWidth}`}
+                alt="Map to Anchorage Location"
+                target="_blank"
+                rel="noopener"
+                title="Click to open Google Maps directions to Anchorage location."
+              />
+            </a>
+          </p>
+        </div>
+        <div className={css.ctaLocationContainer}>
+          <p>
+            <strong>Fairbanks</strong>
+            <a className={css.locationPhone}
+               href={`tel: ${data.call_to_action.fairbanks_phone_number}`}
+               title="Click to call Fairbanks location.">
+              { data.call_to_action.fairbanks_phone_number }
+            </a>
+            <a className={css.locationMap}
+               href={data.call_to_action.fairbanks_google_maps_link}>
+              <img
+                src={`${data.call_to_action.fairbanks_map_image.imgix_url}?w=${imgWidths.mapImgWidth}`}
+                alt="Map to Anchorage Location"
+                target="_blank"
+                rel="noopener"
+                title="Click to open Google Maps directions to Fairbanks location."
+              />
+            </a>
+          </p>
+        </div>
       </div>
     </section>
   </div>
@@ -91,10 +138,13 @@ Index.getInitialProps = async () => {
   const data = request.object.metadata;
 
   // TODO: Find out how to get a better width after the view inits
-  let heroWidth = 1200;
-  let advantageImgWidth = 400;
+  let imgWidths = {
+    heroWidth: 1200,
+    advantageImgWidth: 400,
+    mapImgWidth: 800
+  };
 
-  return {data, heroWidth, advantageImgWidth};
+  return {data, imgWidths};
 }
 
 export default Index;

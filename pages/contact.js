7e2bcfbd7f743c988e3683f21d9539991fce4a4f
Brandon Reid <breid@criticalmix.com>
2018-10-01 07:03:29 -0700

    Wire up contact page tabs

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Cosmic from 'cosmicjs';
 import classNames from 'classnames';
 
@@ -5,56 +6,74 @@ import css from './contact.scss';
 
 import BigContactCard from '../components/big-contact-card/BigContactCard';
 
-const Contact = ({data, activeTab}) => (
-  <div className={css.contactContainer}>
-    <div className={css.containedContact}>
-      <h1>Contact</h1>
-      <div className={css.tabs}>
-        <button
-          className={classNames(
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 'anchorage'
+    }
+
+    this.setActiveTab = this.setActiveTab.bind(this);
+  }
+
+  setActiveTab(location) {
+    this.setState({activeTab: location});
+  }
+
+  static async getInitialProps({ Component, router, ctx }) {
+    const api = Cosmic();
+    const bucket = api.bucket({
+      slug: process.env.COSMIC_BUCKET,
+      read_key: process.env.COSMIC_READ_KEY,
+      write_key: process.env.COSMIC_WRITE_KEY
+    });
+    const request = await bucket.getObject({
+      slug: 'contact'
+    });
+    const data = request.object.metadata;
+  
+    return {data};
+  }
+
+  render() {
+    const { setActiveTab } = this;
+    const { data } = this.props;
+    const { activeTab } = this.state;
+    return (
+      <div className={css.contactContainer}>
+        <div className={css.containedContact}>
+          <h1>Contact</h1>
+          <div className={css.tabs}>
+            <button
+              className={classNames(
+                { [css.active]: activeTab === 'anchorage' }
+              )}
+              onClick={() => setActiveTab('anchorage')}
+            >Anchorage</button>
+            <button
+              className={classNames(
+                { [css.active]: activeTab === 'fairbanks' }
+              )}
+              onClick={() => setActiveTab('fairbanks')}
+            >Fairbanks</button>
+          </div>
+          <div className={classNames(
+            css.tabContent,
             { [css.active]: activeTab === 'anchorage' }
-          )}
-          onClick={() => {activeTab = 'anchorage'}}
-        >Anchorage</button>
-        <button
-          className={classNames(
+          )}>
+            <BigContactCard data={data.anchorage} locationName='Anchorage' />
+          </div>
+
+          <div className={classNames(
+            css.tabContent,
             { [css.active]: activeTab === 'fairbanks' }
-          )}
-          onClick={() => {activeTab = 'fairbanks';}}
-        >Fairbanks</button>
+          )}>
+            <BigContactCard data={data.fairbanks} locationName='Fairbanks' />
+          </div>
+        </div>
       </div>
-      <div className={classNames(
-        css.tabContent,
-        { [css.active]: activeTab === 'anchorage' }
-      )}>
-        <BigContactCard data={data.anchorage} locationName='Anchorage' />
-      </div>
-
-      <div className={classNames(
-        css.tabContent,
-        { [css.active]: activeTab === 'fairbanks' }
-      )}>
-        <BigContactCard data={data.fairbanks} locationName='Fairbanks' />
-      </div>
-    </div>
-  </div>
-);
-
-Contact.getInitialProps = async () => {
-  const api = Cosmic();
-  const bucket = api.bucket({
-    slug: process.env.COSMIC_BUCKET,
-    read_key: process.env.COSMIC_READ_KEY,
-    write_key: process.env.COSMIC_WRITE_KEY
-  });
-  const request = await bucket.getObject({
-    slug: 'contact'
-  });
-  const data = request.object.metadata;
-
-  let activeTab = 'anchorage';
-
-  return {data, activeTab};
+    )
+  }
 }
 
 export default Contact;

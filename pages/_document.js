1fd9865bd762d3f22501ccf5449c2834ea9efb7c
Brandon Reid <breid@criticalmix.com>
2018-10-30 17:01:36 -0700

    Interum commit

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,900" rel="stylesheet" />
 
-          <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+          <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
         </Head>
         <body>
